3645221c3b536a3c19a591678ce583ceac28146d
Zenab Hamdan <hamdanzen866@gmail.com>
2024-11-01 12:30:06 +0300

    Save changes to main.dart before switching branches

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
             fontFamily: 'Poppins',
             useMaterial3: true,
           ),
-           home: NavigationBarScreen(),
+          home: const NavigationBarScreen(),
         );
       },
     );
